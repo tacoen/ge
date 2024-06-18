1d951451c602fa502808e8a4c7049ab6a320d552
tacoen <tacoen@gmail.com>
2024-06-18 14:50:57 +0900

    c 1

--- ge2 - Copy (2)/json.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Content-type: application/json');
-
-$input = file_get_contents('php://input');
-
-file_put_contents('/var/www/ge2/doc/data/content.txt',$input);
-			
-echo json_encode( $input, true );
